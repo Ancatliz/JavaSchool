c4957d4f4c96252182a2cb80e910e91ec01d3fac
Ancatliz <mister.83.30@mail.ru>
2022-12-22 18:27:02 +0300

    Initial commit

---
+++ src/lesson_01/homework_01/Homework_01.java
@@ -0,0 +1,17 @@
+package lesson_01.homework_01;
+
+public class Homework_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int a = 5; // 32 - битное целое число (также есть тоже самое другой битности bite, short, long)
+        System.out.println(a);
+        double b = 7.2;  // 64 - битное значение с плавающей точкой (также есть 32 - битное float
+        System.out.println(b);
+        boolean c = a < b; // истинное или ложное значение
+        System.out.println(c);
+        char d = 'S'; // 16 - битное значение в системе unicode
+        System.out.println(d);
+
+    }
+}
